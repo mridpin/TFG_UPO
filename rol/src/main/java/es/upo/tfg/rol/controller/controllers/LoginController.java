1bc430d25e5f7c56b3bb0a45bfcccddec846870c
mridpin <mridao99@gmail.com>
2018-07-01 12:59:37 +0200

    wip login

---
+++ rol/src/main/java/es/upo/tfg/rol/controller/controllers/LoginController.java
@@ -0,0 +1,13 @@
+package es.upo.tfg.rol.controller.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+
+}
