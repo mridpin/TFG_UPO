c3370e01658a582e938acf725cd57258b331e548
mridpin <mridao99@gmail.com>
2018-08-30 11:12:54 +0200

    done implementacion

--- rol/src/main/java/es/upo/tfg/rol/model/dao/UserRepository.java
+++ rol/src/main/java/es/upo/tfg/rol/model/dao/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 import es.upo.tfg.rol.model.pojos.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-	// Return Optional<T> to comply with CrudRepository standards
+	
 	public Optional<User> findByNickname(final String nickname);
 
 	public Optional<User> findByNicknameAndPassword(final String nickname,
